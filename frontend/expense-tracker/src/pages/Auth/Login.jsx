ef384f17045d7ed0de916697c30c5666aa98a4b7
Vinali Samarasinghe <vinalisamarasinghe@gmail.com>
2025-11-26 15:45:10 +0530

    update loginApi section

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -3,6 +3,8 @@ import AuthLayout from '../../components/layouts/AuthLayout'
 import Input from '../../components/Inputs/Input'
 import { Link, useNavigate } from 'react-router-dom';
 import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -24,7 +26,24 @@ const Login = () => {
     setError("");
     
     //Login API call
+    try{
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password
+      });
+      const {token, user} = response.data;
 
+      if(token){
+        localStorage.setItem("token", token)
+        navigate ("/dashboard");
+      }
+    }catch(error){
+      if(error.response && error.response.data.message){
+        setError(error.response.data.message)
+      }else{
+        setError("Something went wrong, Please try again..");
+      }
+    }
   };
 
   return (
